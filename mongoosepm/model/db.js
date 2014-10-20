2f8fca4bda5ba2df8623b5dacf0154537188e41f
Doua Thao <douathoj@gmail.com>
2014-10-20 16:04:30 -0500

    add user and project schema and build the model

--- mongoosepm/model/db.js
+++ mongoosepm/model/db.js
@@ -26,3 +26,32 @@ process.on('SIGINT', function() {
     process.exit(0);
   });
 });
+
+/*********************************************
+ * USER SCHEMA
+ *********************************************/
+var userSchema = new mongoose.Schema({
+  name: String,
+  email: {type: String, unique: true},
+  createdOn: { type: Date, default: Date.now },
+  modifiedOn: Date,
+  lastLogin: Date
+});
+
+// Build the User model
+mongoose.model('User', userSchema);
+
+/*********************************************
+ * PROJECT SCHEMA
+ *********************************************/
+var projectSchema = new mongoose.Schema({
+  projectName: String,
+  createdOn: { type: Date, default: Date.now },
+  modifiedOn: Date,
+  createdBy: String,
+  contributors: String,
+  tasks: String
+});
+
+// Build the Project model
+mongoose.model('Project', projectSchema);
