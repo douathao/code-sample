3ee91a7210d8f24002262dc6d3f7a66cf63ce18b
Doua Thao <douathoj@gmail.com>
2014-10-21 01:46:31 -0500

    add routes and views

--- mongoosepm/routes/user.js
+++ mongoosepm/routes/user.js
@@ -1,10 +1,57 @@
-var mongoose = require( 'mongoose' );
-var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+//var router = express.Router();
+//
+///* GET users listing. */
+//router.get('/', function(req, res) {
+//  res.send('respond with a resource');
+//});
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
+//module.exports = router;
 
-module.exports = router;
+// GET user creation form
+exports.create = function(req, res) {
+  res.render('user-form', {
+    title: 'Create user',
+    buttonText: 'Join!'
+  });
+};
+
+// POST new user creation form
+exports.doCreate = function(req, res) {
+  User.create({
+    name: req.body.FullName,
+    email: req.body.Email,
+    modifiedOn: Date.now(),
+    lastLogin: Date.now()
+  }, function(err, user) {
+    if (err) {
+      console.log(err);
+      if (err.code === 11000) {
+        res.redirect('/user/new?exists=true');
+      } else {
+        res.redirect('/?error=true');
+      }
+    } else {
+      // Success
+      console.log('User created and saved: ' + user);
+      req.session.user = { 'name': user.name, 'email': user.email, '_id': user._id };
+      req.session.loggedIn = true;
+      res.redirect('/user');
+    }
+  });
+};
+
+// GET logged in user page
+exports.index = function(req, res) {
+  if (req.session.loggedIn === true) {
+    res.render('user-page', {
+      title: req.session.user.name,
+      name: req.session.user.name,
+      email: req.session.user.email,
+      userID: req.session.user._id
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
