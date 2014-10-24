e198d796dc61c1679433c164b20c2fb7af69e4ed
Doua Thao <douathoj@gmail.com>
2014-10-23 23:49:06 -0500

    refactor to use beforeEach

--- spec/NumbersSpec.js
+++ spec/NumbersSpec.js
@@ -1,4 +1,4 @@
-/* globals describe, define, it, expect */
+/* globals describe, define, it, expect, beforeEach */
 define([
   'Numbers'
 ], function(
@@ -9,23 +9,29 @@ define([
   describe('The Number class', function () {
     // nested suite
     describe('The add function', function () {
-      it('should add one or more arguments and return the sum of them', function () {
-        // arrange
-        var input1 = 1;
-        var input2 = 2;
+      var output;
 
+      // run before each test
+      beforeEach(function () {
+        // arrange
+        this.numberInput1 = 1;
+        this.numberInput2 = 2;
+        this.stringInput1 = '1';
+      });
+
+      // run after each test
+      // afterEach...
+
+      it('should add one or more arguments and return the sum of them', function () {
         // act
-        var output = Numbers.add(input1, input2);
+        output = Numbers.add(this.numberInput1, this.numberInput2);
 
         // assert
         expect(output).toEqual(3);
       });
 
       it('should try to parse an interger when a string is passed to the function', function () {
-        var input1 = 1;
-        var input2 = '2';
-
-        var output = Numbers.add(input1, input2);
+        output = Numbers.add(this.stringInput1, this.numberInput2);
 
         expect(output).toEqual(3);
       });
